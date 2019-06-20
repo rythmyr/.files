16dad6a08c32fd781d9a1e714294ef5b09461239
Rythmyr <rythmyr@gmail.com>
2019-06-20 00:20:08 -0500

    emacs - magit, editorconfig, backups dir fix

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,8 @@
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+(setq mac-control-modifier 'control)
+
+
 ;; set up package stuff
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -9,19 +14,3 @@
   (package-install 'use-package))
 
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-idle-delay 0)
- '(package-selected-packages
-   (quote
-    (omnisharp use-package tide solarized-theme projectile org-evil evil-org company)))
- '(tide-tsserver-executable "node_modules/typescript/bin/tsserver" t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
