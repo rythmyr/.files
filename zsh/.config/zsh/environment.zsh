3aabbc4bbbbb58e63e238fb877f30a81dd442045
Rythmyr <rythmyr@gmail.com>
2019-07-25 17:35:48 -0500

    zsh - beginnings of DEFAULT_PATH
    I want to reset my PATH every time I source my .zshrc and build it from scratch
    
    not in .zshenv because on mac, brew(?) doesn't add /usr/local/bin to path until after that's sourced, and that needs included

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -2,7 +2,10 @@
 # STANDARD VARIABLES #
 #====================#
 
-PATH=$HOME/bin:$PATH
+# Don't continually add to $PATH
+# Also works as a way to reset it
+[ -z "$DEFAULT_PATH" ] && export DEFAULT_PATH=$PATH
+PATH=$HOME/bin:$DEFAULT_PATH
 
 export EDITOR=`which nvim`
 
