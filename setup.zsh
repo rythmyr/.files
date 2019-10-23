f8a6a3b7c4a7ba766c97119d9e0bf06e56c6b685
Rythmyr <rythmyr@gmail.com>
2019-10-22 22:17:06 -0500

    i3blocks - add new i3blocks config

--- setup.zsh
+++ setup.zsh
@@ -21,4 +21,4 @@ mkdir -p ~/bin
 
 # now we install all the stuff. Not sure if I want to do all at once or each in its individual operation
 # If one of these fails due to conflict (user already has configuration) it will fail for all of them
-stow compton emacs git i3 neofetch nvim rofi ssh termite tmux zsh
+stow compton emacs git i3 neofetch nvim rofi ssh termite tmux zsh i3blocks
