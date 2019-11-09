2b97c90a7ae1f425460dfa6e61b1ffb0c1a14717
Rythmyr <rythmyr@gmail.com>
2019-11-08 19:26:51 -0600

    zsh - add function for switching projects

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -10,3 +10,16 @@ alias s="RYTH_ZSH_DEBUG=true . ~/.config/zsh/.zshrc"
 alias dot='cd $DOTFILES_GIT'
 alias kb='cd $KEYBASE_HOME'
 alias org='cd ~/data/org'
+
+function pj() {
+    unset PROJECT
+    unset PROJECT_DIR
+
+    export PATH=${PATH_NOPROJ:-$PATH}
+
+    unset PATH_NOPROJ
+
+    cd $HOME/data/projects/$1
+    [[ -e sourceme ]] && source sourceme
+    [[ -d repo ]] && cd repo
+}
