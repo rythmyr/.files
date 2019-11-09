c4257f51661e2f61fe6336331e7c2aa3d95cb03c
Rythmyr <rythmyr@gmail.com>
2019-11-08 19:27:26 -0600

    zsh - change old "s" and "e" aliases to zsh-source and zsh-edit
    I'm not working on editing and re-sourcing my zsh config any more, those letters are better for project aliases
    I may switch them to be shorter than this later, like ce or cr or something, like my vim/emacs config binds

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -4,8 +4,8 @@ alias la='ls --color=auto -A'
 alias lla='ls --color=auto -lhA'
 alias bell='echo -e "\a"'
 
-alias e="$EDITOR ~/.config/zsh/.zshrc"
-alias s="RYTH_ZSH_DEBUG=true . ~/.config/zsh/.zshrc"
+alias zsh-edit="$EDITOR $ZDOTDIR/.zshrc"
+alias zsh-source="RYTH_ZSH_DEBUG=true . $ZDOTDIR/.zshrc"
 
 alias dot='cd $DOTFILES_GIT'
 alias kb='cd $KEYBASE_HOME'
