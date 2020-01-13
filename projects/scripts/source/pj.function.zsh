03f84472d5559321cde561ad12e1d2a4edcd2204
Rythmyr <rythmyr@gmail.com>
2020-01-13 10:02:35 -0600

    projects - update pj function to not use the sourceme file
    everything should be done inside the function instead.
    Easier to maintain this way, almost everything related to projects is in this one file now, and the templates

--- projects/scripts/source/pj.function.zsh
+++ projects/scripts/source/pj.function.zsh
@@ -1,14 +1,24 @@
 #!/usr/bin/env zsh
 
 function pj() {
+    # unset anything set in this function, resetting PATH if necessary
     unset PROJECT
     unset PROJECT_DIR
 
-    export PATH=${PATH_NOPROJ:-$PATH}
+    export PATH="${PATH_NOPROJ:-$PATH}"
 
     unset PATH_NOPROJ
 
-    cd $HOME/data/projects/$1
-    [[ -s sourceme ]] && source sourceme
-    [[ -d repo ]] && cd repo
+    # go to the directory (or the projects directory if not provided)
+    cd ${PROJECTS_DIR:-"$HOME/data/projects/"}$1
+
+    if [[ $1 ]]; then
+        export PROJECT=$1
+        export PROJECT_DIR=$PWD
+
+        export PATH_NOPROJ="${PATH_NOPROJ:-$PATH}"
+        export PATH="$PWD:$PATH_NOPROJ"
+
+        [[ -d repo ]] && cd repo
+    fi
 }
