81fa8b7e5e5a7e0c01af1a95bdad44a457757db2
Rythmyr <rythmyr@gmail.com>
2020-03-28 13:43:15 -0500

    scripts - update lock script
    -m is no longer supported, have to use the full --pass-media-keys flag
    
    also converted to using a string for args so I can easily comment/uncomment flags if I want

--- scripts/scripts/lock.zsh
+++ scripts/scripts/lock.zsh
@@ -8,34 +8,40 @@ W='#880000bb'  # wrong
 V='#bb00bbbb'  # verifying
 BG='#301934'   # background color
 
-i3lock \
---insidevercolor=$C   \
---ringvercolor=$V     \
-\
---insidewrongcolor=$C \
---ringwrongcolor=$W   \
-\
---insidecolor=$C      \
---ringcolor=$D        \
---linecolor=$B        \
---separatorcolor=$D   \
-\
---verifcolor=$T       \
---wrongcolor=$T       \
---timecolor=$T        \
---datecolor=$T        \
---layoutcolor=$T      \
---keyhlcolor=$W       \
---bshlcolor=$W        \
-\
---screen 1            \
---color=$BG             \
---clock               \
---indicator           \
---timestr="%H:%M:%S"  \
---datestr="%B %d, %Y" \
---keylayout 2         \
--m                    \
+local args=''
+
+
+args+=" \"--insidevercolor=$C\""
+args+=" \"--ringvercolor=$V\""
+
+args+=" \"--insidewrongcolor=$C\""
+args+=" \"--ringwrongcolor=$W\""
+args+=" \"--insidecolor=$C\""
+args+=" \"--ringcolor=$D\""
+args+=" \"--linecolor=$B\""
+args+=" \"--separatorcolor=$D\""
+
+args+=" \"--verifcolor=$T\""
+args+=" \"--wrongcolor=$T\""
+args+=" \"--timecolor=$T\""
+args+=" \"--datecolor=$T\""
+args+=" \"--layoutcolor=$T\""
+args+=" \"--keyhlcolor=$W\""
+args+=" \"--bshlcolor=$W\""
+
+args+=" \"--screen=1\""
+args+=" \"--color=$BG\""
+args+=" \"--clock\""
+args+=" \"--indicator\""
+args+=" \"--timestr=%H:%M:%S\""
+args+=" \"--datestr=%B %d, %Y\""
+args+=" \"--keylayout=2\""
+args+=" \"--pass-media-keys\""
+args+=" \"--ignore-empty-password\""
+
+echo "i3lock $args"
+
+eval "i3lock $args"
 
 # --veriftext="Drinking verification can..."
 # --wrongtext="Nope!"
