baebb06b9e9f14a1090e76afe48590552ba9ee2f
Rythmyr <rythmyr@gmail.com>
2023-03-14 16:56:09 -0500

    zsh - use generic $NVM_DIR instead of specific folder names for nvm

--- zsh/.config/zsh/programs.zsh
+++ zsh/.config/zsh/programs.zsh
@@ -4,6 +4,8 @@
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. /usr/local/opt/nvm/nvm.sh
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. /usr/local/opt/nvm/etc/bash_completion.d/nvm
 
+[ -s "$NVM_DIR/nvm.sh" ] && \. $NVM_DIR/nvm.sh
+
 if [[ $NODE_USE_VERSION ]] && command -v nvm >/dev/null 2>&1; then
     nvm use $NODE_USE_VERSION >/dev/null
 fi
